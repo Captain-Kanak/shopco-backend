86fb4b926f1dc7e45992d35f55bdd4b323030f6e
Captain-Kanak <kanakroy835@gmail.com>
2026-09-10 23:40:34 +0600

    get first response from server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+import status from "http-status";
+import { sendResponse } from "./utils/send-response.js";
 
 const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  return sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "ShopCo Server is Running Successfully",
+  });
+});
 
 export default app;
---
+++ src/utils/send-response.ts
@@ -0,0 +1,20 @@
+import { Response } from "express";
+
+interface ResponseData<T = unknown> {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  data?: T;
+  error?: unknown;
+}
+
+export const sendResponse = <T>(res: Response, resData: ResponseData<T>) => {
+  const { statusCode, success, message, data, error } = resData;
+
+  return res.status(statusCode).json({
+    success,
+    message,
+    data,
+    error,
+  });
+};
