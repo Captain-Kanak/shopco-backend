0a0b5edc334c7afe35faa6e57a24461b285aa320
Captain-Kanak <kanakroy835@gmail.com>
2026-09-11 20:37:11 +0600

    setup view ejs engine for cutomize email templates

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,30 @@
-import express, { Application, json, Request, Response } from "express";
+import express, {
+  Application,
+  json,
+  Request,
+  Response,
+  urlencoded,
+} from "express";
 import status from "http-status";
 import { sendResponse } from "./utils/send-response.js";
 import { indexRouter } from "./routes/index.js";
 import errorMiddleware from "./middlewares/error-middleware.js";
+import path from "path";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
-// json middleware
+// view engine setup
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "src/templates"));
+
+// query parser
+app.set("query parser", "extended");
+
+// middlewares
 app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   return sendResponse(res, {
