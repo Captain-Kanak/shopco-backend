8db330b1523105ba86bd6d0087612ad5d00019e2
Captain-Kanak <kanakroy835@gmail.com>
2026-09-23 11:50:43 +0600

    all cart routes test successfully

--- src/modules/cart/cart.route.ts
+++ src/modules/cart/cart.route.ts
@@ -1,14 +1,26 @@
 import { Router } from "express";
 import { cartController } from "./cart.controller.js";
 import { authMiddleware } from "../../middlewares/auth-middleware.js";
+import { validateRequestBody } from "../../middlewares/zod-middleware.js";
+import { cartValidation } from "./cart.validation.js";
 
 const router = Router();
 
-router.post("/", authMiddleware(), cartController.addToCart);
+router.post(
+  "/",
+  authMiddleware(),
+  validateRequestBody(cartValidation.addToCart),
+  cartController.addToCart,
+);
 
 router.get("/", authMiddleware(), cartController.getCarts);
 
-router.patch("/:id", authMiddleware(), cartController.updateCartQuantity);
+router.patch(
+  "/:id",
+  authMiddleware(),
+  validateRequestBody(cartValidation.updateCartQuantity),
+  cartController.updateCartQuantity,
+);
 
 router.delete("/:id", authMiddleware(), cartController.removeFromCart);
 
